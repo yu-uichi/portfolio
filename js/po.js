c34e08ff1388833e03d3c09220f8b7598a269b8e
yu-uichi <86936477+yu-uichi@users.noreply.github.com>
2021-07-23 13:37:41 +0900

    js追加

---
+++ js/po.js
@@ -0,0 +1,79 @@
+const image01src = 'url("img/image001.jpg")';
+const image02src = 'url("img/image002.jpg")';
+const image03src = 'url("img/image003.jpg")';
+const image04src = 'url("img/image004.jpg")';
+const image05src = 'url("img/image005.jpg")';
+const image06src = 'url("img/image006.jpg")';
+const image07src = 'url("img/image007.jpg")';
+const image08src = 'url("img/image008.jpg")';
+function nextImage() {
+  const header = document.getElementById('top');
+  const nowBg = header.style.backgroundImage;
+  console.log(nowBg);
+
+  if (nowBg == image01src) {
+    // 今1枚め
+    header.style.backgroundImage = image02src;
+  } else if (nowBg == image02src) {
+    // 今2枚め
+    header.style.backgroundImage = image03src;
+  } else if (nowBg == image03src) {
+    // 今3枚め
+    header.style.backgroundImage = image04src;
+  } else if (nowBg == image04src) {
+    // 今4枚め
+    header.style.backgroundImage = image05src;
+  } else if (nowBg == image05src) {
+    // 今5枚め
+    header.style.backgroundImage = image06src;
+  } else if (nowBg == image06src) {
+    // 今6枚め
+    header.style.backgroundImage = image07src;
+  } else if (nowBg == image07src) {
+    // 今7枚め
+    header.style.backgroundImage = image08src;
+  } else {
+    // 今8枚め
+    header.style.backgroundImage = image01src;
+  }
+}
+
+function prevImage() {
+  const header = document.getElementById('top');
+  const nowBg = header.style.backgroundImage;
+  console.log(nowBg);
+
+  if (nowBg == image01src) {
+    // 今1枚め TODO:
+    header.style.backgroundImage = image08src;
+  } else if (nowBg == image02src) {
+    // 今2枚め TODO:
+    header.style.backgroundImage = image01src;
+  } else if (nowBg == image03src) {
+    // 今3枚め TODO:
+    header.style.backgroundImage = image02src;
+  } else if (nowBg == image04src) {
+    // 今4枚め TODO:
+    header.style.backgroundImage = image03src;
+  } else if (nowBg == image05src) {
+    // 今5枚め TODO:
+    header.style.backgroundImage = image04src;
+  } else if (nowBg == image06src) {
+    // 今6枚め TODO:
+    header.style.backgroundImage = image05src;
+  } else if (nowBg == image07src) {
+    // 今7枚め TODO:
+    header.style.backgroundImage = image06src;
+  } else {
+    // 今8枚め TODO:
+    header.style.backgroundImage = image07src;
+  }
+}
+
+// ページが表示されたタイミングで実行したい処理を書く
+document.getElementById('top').style.backgroundImage = image01src;
+document.getElementById('sc-to-left').addEventListener('click', prevImage);
+document.getElementById('sc-to-right').addEventListener('click', nextImage);
+
+// タイマーセット
+setInterval(nextImage, 5000);
